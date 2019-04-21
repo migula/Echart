d476d2cfa96d1987780f56749c511ae2e46dd2fb
migula <cool187@126.com>
2019-04-21 17:18:28 +0800

    数据legend处理

--- src/components/echart.vue
+++ src/components/echart.vue
@@ -4,17 +4,8 @@
 
    
 <script>
-//请求到这样的数据,让你渲染到统计图
-let data = [
-  {
-    Month: "1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月",
-    zhengFaLiang: " 2.0,4.9,7.0,23.2,25.6,76.7,135.6,162.2,32.6,20.0,6.4,3.3",
-    jiangShuiLiang: "2.6,5.9,9.0,26.4,28.7,70.7,175.6,182.2,48.7,18.8,6.0,2.3",
-    pingJunWenDu: "2.0,2.2,3.3,4.5,6.3,10.2,20.3,23.4,23.0,16.5,12.0,6.2"
-  }
-];
 export default {
-  name: "main",
+  name: "echart",
   props: {
     msg: String
   },
@@ -39,12 +30,25 @@ export default {
           }
         },
         legend: {
-          data: ["蒸发量", "降水量", "平均温度"]
+          data: []
         },
         xAxis: [
           {
             type: "category",
-            data: [],
+            data: [
+              "1月",
+              "2月",
+              "3月",
+              "4月",
+              "5月",
+              "6月",
+              "7月",
+              "8月",
+              "9月",
+              "10月",
+              "11月",
+              "12月"
+            ],
             axisPointer: {
               type: "shadow"
             }
@@ -73,26 +77,62 @@ export default {
           }
         ],
         series: [
-          // {
-          //   stack:['蒸发量','降水量']
-          // },
           {
-            name: "蒸发量",
+            name: "zhengFaLiang",
             type: "bar",
-            stack: ["蒸发量", "降水量"],
-            data: []
+            stack: ["蒸发量", "降水量"], //这个只是简单拼在一起,实际效果需要重叠,待解决
+            data: [
+              2.0,
+              4.9,
+              7.0,
+              23.2,
+              25.6,
+              76.7,
+              135.6,
+              162.2,
+              32.6,
+              20.0,
+              6.4,
+              3.3
+            ]
           },
           {
-            name: "降水量",
-            stack: ["蒸发量", "降水量"],
+            name: "jiangShuiLiang",
             type: "bar",
-            data: []
+            stack: ["蒸发量", "降水量"], //这个只是简单拼在一起,实际效果需要重叠,待解决
+            data: [
+              2.6,
+              5.9,
+              9.0,
+              26.4,
+              28.7,
+              70.7,
+              175.6,
+              182.2,
+              48.7,
+              18.8,
+              6.0,
+              2.3
+            ]
           },
           {
-            name: "平均温度",
+            name: " pingJunWenDu",
             type: "line",
             yAxisIndex: 1,
-            data: []
+            data: [
+              2.0,
+              2.2,
+              3.3,
+              4.5,
+              6.3,
+              10.2,
+              20.3,
+              23.4,
+              23.0,
+              16.5,
+              12.0,
+              6.2
+            ]
           }
         ]
       }
@@ -109,6 +149,25 @@ export default {
       // var myChart = this.echarts.init(document.getElementById('main'));
       let myChart = this.echarts.init(this.$refs.edata);
 
+      let data = [
+        {
+          Month: "1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月",
+          zhengFaLiang:
+            " 2.0,4.9,7.0,23.2,25.6,76.7,135.6,162.2,32.6,20.0,6.4,3.3",
+          jiangShuiLiang:
+            "2.6,5.9,9.0,26.4,28.7,70.7,175.6,182.2,48.7,18.8,6.0,2.3",
+          pingJunWenDu: "2.0,2.2,3.3,4.5,6.3,10.2,20.3,23.4,23.0,16.5,12.0,6.2"
+        }
+      ];
+      let tempData = data[0];
+      for (let key in tempData) {
+        console.log(key, ".....key");
+        this.option.legend.data.push(key);
+        console.log( this.option.legend.data.push(key),' this.option.legend.data.push(key)');
+        //键值
+        // console.log(tempData[key]);
+      }
+      
       myChart.setOption(this.option);
     }
   }
